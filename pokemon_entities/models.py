fda632cfba2515079fb1180114e00dc4fb880bc1
Anton <56098143+ShvetsovAnton@users.noreply.github.com>
2023-02-26 18:30:23 +0400

    announced mandatory fields to fill in the django admin panel

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -8,15 +8,21 @@ class Pokemon(models.Model):
         verbose_name='Из кого эволюционировал',
         on_delete=models.CASCADE,
         related_name='next_evolutions',
-        null=True,
-        blank=True
+        blank=True,
+        null=True
     )
     title = models.CharField('Имя покемона RU', max_length=200, default='')
-    title_eng = models.CharField('Имя покемона ENG', max_length=200, default='')
-    title_jp = models.CharField('Имя покемона JP', max_length=200, default='')
-    photo = models.ImageField('Изображение покемона', blank=True, default=None)
-    appear = models.DateTimeField('Появиться', blank=True, null=True)
-    disappear = models.DateTimeField('Исчезнет', blank=True, null=True)
+    title_eng = models.CharField(
+        'Имя покемона ENG',
+        max_length=200
+    )
+    title_jp = models.CharField(
+        'Имя покемона JP',
+        max_length=200,
+    )
+    photo = models.ImageField('Изображение покемона')
+    appear = models.DateTimeField('Появиться')
+    disappear = models.DateTimeField('Исчезнет')
     leve = models.IntegerField('Уровень', blank=True, null=True)
     health = models.IntegerField('Здоровье', blank=True, null=True)
     strength = models.IntegerField('Атака', blank=True, null=True)
@@ -34,8 +40,8 @@ class PokemonEntity(models.Model):
         Pokemon,
         verbose_name='Выберите покемона',
         on_delete=models.CASCADE)
-    lat = models.FloatField('Широта', blank=True)
-    lon = models.FloatField('Долгота', blank=True)
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
 
     def __str__(self):
         return f'Координаты: Широта - {self.lat}, Долгота - {self.lon}'
