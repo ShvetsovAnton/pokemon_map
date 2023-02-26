d6707f28d14225f7b003d4e025a222cfd8d9cea9
Anton <56098143+ShvetsovAnton@users.noreply.github.com>
2023-02-26 18:39:51 +0400

    deleted unused data from the project.

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -8,11 +8,6 @@ from .models import (Pokemon,
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
-DEFAULT_IMAGE_URL = (
-    'https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision'
-    '/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832'
-    '&fill=transparent'
-)
 
 
 def take_pokemon_entity(pokemon, request, folium_map):
@@ -123,7 +118,7 @@ def take_pokemon_description(request, pokemon):
     return pokemon_description
 
 
-def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
+def add_pokemon(folium_map, lat, lon, image_url):
     icon = folium.features.CustomIcon(
         image_url,
         icon_size=(50, 50),
@@ -170,4 +165,3 @@ def show_pokemon(request, pokemon_id):
         'map': folium_map._repr_html_(),
         'pokemon': pokemon_description
     })
-
